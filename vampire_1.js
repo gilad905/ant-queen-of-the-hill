16103986ed5945649cf5ad34ef8b85590fdedb17
Gilad Mayani <gilad905@gmail.com>
2017-08-23 00:25:52 +0200

    .

--- vampire_1.js
+++ vampire_1.js
@@ -16,12 +16,9 @@ go on until finding another queen
 start:
 glide around until finding food, make 1 worker
 continue gliding until finding any color
-if it's a spot - find its center
-if it's a trail - follow it
-favor spots over trails (higher chance that the queen is idle)
-trace the queen
-steal from it with the worker, until it's empty
-go on until finding another queen
+gravitate towards color's mass center
+
+----------------------------------------------------
 
 */
 
@@ -35,131 +32,138 @@ var RED = 5;
 var myCell = view[4];
 var me = myCell.ant;
 
-function forQueen() {
-   if (myCell.color == WHITE)
-      return MarkMe(RED);
-   else {
-      var friendAround = FindAround("friend");
-      var redPos = FindAround("color", RED);
-      var oppRed = OppositeTo(redPos);
+return Validate(me.type == QUEEN ? forQueen() : forWorker());
 
-      if (me.food && friendAround == -1) {
-         if (redPos != -1 && view[oppRed].color != RED) {
-            var workerPos = NewWorker(redPos);
-            return CreateAt(workerPos);
-         } else
-            return MoveTo(4);
-      } else {
-         if (redPos != -1) {
-            return MoveTo(oppRed);
-         } else
-            return MoveTo(0);
-      }
-   }
+function forQueen() {
+    var worker = FindAround("friend");
+
+    if (worker == -1) {
+        if (me.food)
+            return CreateAt(3);
+        else {
+            var foodPos = FindAround("food");
+            if (foodPos != -1) {
+                console.pause();
+                return MoveTo(foodPos);
+            }
+            else
+                return RedTrail();
+        }
+    } else
+        return MoveTo(RightOf(worker));
 }
 
 function forWorker() {
-   if (myCell.color == WHITE)
-      return MarkMe(YELLOW);
-   else {
-      var queenPos = FindAround("friend");
-      var redPos = FindAround("color", RED);
-      var toGo = ContinuePath(queenPos, redPos);
-      return MoveTo(toGo == -1 ? 4 : toGo);
-   }
+    var queen = FindAround("friend");
+    return MoveTo(LeftOf(queen));
 }
-
-return Validate(me.type == QUEEN ? forQueen() : forWorker());
 
 ///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
-function NewWorker(redPos) {
-   if (redPos == 0 || redPos == 2)
-      return 7;
-   else
-      return 1;
+function RedTrail() {
+    if (myCell.color != RED)
+        return MarkMe(RED);
+    else {
+        var redPos = FindAround("color", RED);
+        if (redPos != -1) {
+            var oppRed = OppositeTo(redPos);
+            return MoveTo(oppRed);
+        } else
+            return MoveTo(2);
+    }
 }
 
-function ContinuePath(queenPos, redPos) {
-   var toGo = -1;
-   if (queenPos == 1)
-      toGo = redPos == 3 ? 2 : 0;
-   else if (queenPos == 3)
-      toGo = redPos == 1 ? 6 : 0;
-   else if (queenPos == 5)
-      toGo = redPos == 1 ? 8 : 2;
-   else if (queenPos == 7)
-      toGo = redPos == 5 ? 6 : 8;
-   return toGo;
+function LeftOf(pos) {
+    if (pos == 8 || pos == 5)
+        return pos - 3;
+    else if (pos == 0 || pos == 3)
+        return pos + 3;
+    else if (pos == 6 || pos == 7)
+        return pos + 1;
+    else if (pos == 1 || pos == 2)
+        return pos - 1;
+}
+
+function RightOf(pos) {
+    if (pos == 3 || pos == 6)
+        return pos - 3;
+    else if (pos == 2 || pos == 5)
+        return pos + 3;
+    else if (pos == 0 || pos == 1)
+        return pos + 1;
+    else if (pos == 7 || pos == 8)
+        return pos - 1;
 }
 
 function Validate(move) {
-   if (
-      (!move) ||
-      (move.cell === undefined || move.cell < 0 || move.cell > 8) ||
-      (move.color < 1 || move.color > 8) ||
-      (move.type < 1 || move.type > 4) ||
-      (move.color && move.type) ||
-      (!move.color && !move.type && move.cell != 4 && view[move.cell].ant)
-   ) {
-      // console.error("INVALID MOVE:");
-      // console.error(move);
-      // console.pause();
-      return MoveTo(4);
-   }
+    if (
+        (!move) ||
+        (move.cell === undefined || move.cell < 0 || move.cell > 8) ||
+        (move.color < 1 || move.color > 8) ||
+        (move.type < 1 || move.type > 4) ||
+        (move.color && move.type) ||
+        (move.type && view[move.cell].ant) ||
+        (!move.color && !move.type && move.cell != 4 && view[move.cell].ant)
+    ) {
+        // console.error("INVALID MOVE:");
+        // console.error(move);
+        // console.pause();
+        return MoveTo(4);
+    }
 
-   return move;
+    return move;
 }
 
 function OppositeTo(cell) {
-   return 8 - cell;
+    return 8 - cell;
 }
 
 function CreateAt(cell, type) {
-   type = type || WORKER;
-   return {
-      cell: cell,
-      type: type,
-   };
+    type = type || WORKER;
+    return {
+        cell: cell,
+        type: type,
+    };
 }
 
 function MarkMe(color) {
-   return {
-      cell: 4,
-      color: color,
-   };
+    return {
+        cell: 4,
+        color: color,
+    };
 }
 
 function MoveTo(cell) {
-   return {
-      cell: cell,
-   };
+    return {
+        cell: cell,
+    };
 }
 
 function FindAround(type, value) {
-   for (var i = 0, cell; cell = view[i]; i++) {
-      if (i != 4 && CellMatchesDesc(cell, type, value))
-         return i;
-   }
-   return -1;
+    for (var i = 0, cell; cell = view[i]; i++) {
+        if (i != 4 && CellMatchesDesc(cell, type, value))
+            return i;
+    }
+    return -1;
 }
 
 function CountAround(type, value) {
-   var count = 0;
-   for (var i = 0, cell; cell = view[i]; i++) {
-      if (i != 4 && CellMatchesDesc(cell, type, value))
-         count++;
-   }
-   return count;
+    var count = 0;
+    for (var i = 0, cell; cell = view[i]; i++) {
+        if (i != 4 && CellMatchesDesc(cell, type, value))
+            count++;
+    }
+    return count;
 }
 
 function CellMatchesDesc(cell, type, value) {
-   if (type == "friend" && cell.ant && cell.ant.friend)
-      return true;
-   else if (type == "color" && cell.color == value)
-      return true;
-   else if (type == "food" && cell.food)
-      return true;
+    if (
+        (type == "friend" && cell.ant && cell.ant.friend) ||
+        (type == "color" && (cell.color == value || !value && cell.color != WHITE)) ||
+        (type == "food" && cell.food)
+    )
+        return true;
 
-   return false;
+    return false;
 }
